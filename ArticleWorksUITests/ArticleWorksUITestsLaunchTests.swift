9c7a35242dc8d2c5984e02a068d7db77526ac47f
sophia <sophia.munoz@>
2023-02-02 12:21:33 -0300

    Initial Commit

---
+++ ArticleWorksUITests/ArticleWorksUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ArticleWorksUITestsLaunchTests.swift
+//  ArticleWorksUITests
+//
+//  Created by Sophia Paulette Muñoz Pailamilla on 02-02-23.
+//
+
+import XCTest
+
+final class ArticleWorksUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
