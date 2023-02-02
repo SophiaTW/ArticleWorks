c998e97969cde0736d65f5ada7248e69dfc961d5
Paula Daniela Nieto Ponce <paula.nieto@Paulas-MacBook-Pro.local>
2023-02-02 16:18:09 -0300

    Create Home Page with navigation Tab Bar

---
+++ ArticleWorks/Controllers/TabBar.swift
@@ -0,0 +1,39 @@
+//
+//  TabBar.swift
+//  ArticleWorks
+//
+//  Created by Paula Daniela Nieto Ponce on 02-02-23.
+//
+
+import UIKit
+
+class TabBar: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        UITabBar.appearance().barTintColor = .systemBackground
+        tabBar.tintColor = .label
+        setupVCs()
+    }
+    
+    fileprivate func createNavController(for rootViewController: UIViewController,
+                                         title: String,
+                                         image: UIImage) -> UIViewController {
+        let navController = UINavigationController(rootViewController: rootViewController)
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = image
+        navController.navigationBar.prefersLargeTitles = true
+        rootViewController.navigationItem.title = title
+        return navController
+    }
+    
+    func setupVCs() {
+        viewControllers = [
+            createNavController(for: ViewController(), title: NSLocalizedString("My Tutorials", comment: ""), image: UIImage(systemName: "person.crop.circle.fill")!),
+            createNavController(for: ViewController(), title: NSLocalizedString("Downloads", comment: ""), image: UIImage(systemName: "arrow.down.circle.fill")!),
+            createNavController(for: ViewController(), title: NSLocalizedString("Library", comment: ""), image: UIImage(systemName: "play.square.stack.fill")!)
+        ]
+    }
+    
+}
