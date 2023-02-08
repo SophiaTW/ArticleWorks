4fdf4862740a073924ae772f2f800ba959cba932
Paula Daniela Nieto Ponce <paula.nieto@Paulas-MacBook-Pro.local>
2023-02-08 09:45:14 -0300

    tests for article's fields

--- ArticleWorksTests/ArticleAPIUnitTests.swift
+++ ArticleWorksTests/ArticleAPIUnitTests.swift
@@ -12,6 +12,7 @@ import Foundation
 class ArticleAPIUnitTests: XCTestCase {
     var sut: ArticleAPI?
     var data: ArticlesResponseDTO?
+    var expected = [Article(title: "Mac Catalyst with Andy Pereira – Podcast S10 E7", domain: "iOS & Swift", description: "Dru flips the switch on his 50th show with Andy Pereira telling us how easy Catalyst can be. After Dru talks about transitioning from developer to architect. \n", createdDate: "2020-05-20T18:01:43.000Z", duration: 60, image: "https://koenig-media.raywenderlich.com/uploads/2016/02/Logo.png")]
     
     override func setUp() {
         sut = ArticleAPI()
@@ -23,14 +24,40 @@ class ArticleAPIUnitTests: XCTestCase {
     }
     
     // TODO: Probar datos especificos, no especificos, probar todo
-    func testMapDTOToArticle() {
+    func testMapDTOToArticleTitle() {
         // given
-        let expected = [Article(title: "Mac Catalyst with Andy Pereira – Podcast S10 E7", domain: "iOS & Swift", description: "Dru flips the switch on his 50th show with Andy Pereira telling us how easy Catalyst can be. After Dru talks about transitioning from developer to architect. \n", createdDate: "2020-05-20T18:01:43.000Z", duration: 60, image: "https://koenig-media.raywenderlich.com/uploads/2016/02/Logo.png")]
         let articleResponseDTO = data!
         // when
         let result = sut!.mapDTOToArticle (articlesResponseDTO: articleResponseDTO)
         // then
         XCTAssertEqual(expected[0].title, result[0].title)
+    }
+    
+    func testMapDTOToArticleDomain() {
+        // given
+        let articleResponseDTO = data!
+        // when
+        let result = sut!.mapDTOToArticle (articlesResponseDTO: articleResponseDTO)
+        // then
         XCTAssertEqual(expected[0].domain, result[0].domain)
     }
+    
+    func testMapDTOToArticleDescription() {
+        // given
+        let articleResponseDTO = data!
+        // when
+        let result = sut!.mapDTOToArticle (articlesResponseDTO: articleResponseDTO)
+        // then
+        XCTAssertEqual(expected[0].description, result[0].description)
+    }
+    
+    func testMapDTOToArticleImage() {
+        // given
+        let articleResponseDTO = data!
+        // when
+        let result = sut!.mapDTOToArticle (articlesResponseDTO: articleResponseDTO)
+        // then
+        XCTAssertEqual(expected[0].image, result[0].image)
+    }
+    
 }
